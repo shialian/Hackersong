5432f37e91570046fb096051d0dfaa105651c72d
lon0862 <59390771+lon0862@users.noreply.github.com>
2022-05-27 00:07:49 +0800

    Add files via upload

--- Assets/Sctipts/Show_or_not.cs
+++ Assets/Sctipts/Show_or_not.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Show_or_not : MonoBehaviour
 {
     public GameObject obj;
+    public AudioClip audio;
     public float delaytime = 0;
     public void InvokeShow()
     {
+        delaytime = audio.length;
         Invoke("show", delaytime);
     }
     public void InvokeDisappear()
